7572732dec86a6e82db71054d18e1abb30c26394
Jesucristo es Rey <jesucristo@MacBook-Air-de-Jesucristo.local>
2026-02-13 12:31:42 -0200

    fix: add env vars for supabase

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = 'https://vzzjkoviyodurlzsiiup.supabase.co';
-const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6emprb3ZpeW9kdXJsenNpaXVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDk5Mjc2MCwiZXhwIjoyMDg2NTY4NzYwfQ.7d5zzDtJ-JKILPnF44NVpBFKUptlXRyknThyhKoTrtY';
+const SUPABASE_URL = process.env.SUPABASE_URL || 'https://vzzjkoviyodurlzsiiup.supabase.co';
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6emprb3ZpeW9kdXJsenNpaXVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDk5Mjc2MCwiZXhwIjoyMDg2NTY4NzYwfQ.7d5zzDtJ-JKILPnF44NVpBFKUptlXRyknThyhKoTrtY';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
